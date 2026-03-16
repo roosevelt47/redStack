1e1ffeaab492bb34918f070ed9b95992389c83a2
BaddKharma <mike.ortiz.us@gmail.com>
2026-03-16 17:54:27 -0400

    fix redirector subnet CIDR to derive from var.redirector_vpc_cidr

--- redirector.tf
+++ redirector.tf
@@ -18,7 +18,7 @@ resource "aws_vpc" "redirector" {
 
 resource "aws_subnet" "redirector" {
   vpc_id                  = aws_vpc.redirector.id
-  cidr_block              = "10.60.1.0/24"
+  cidr_block              = cidrsubnet(var.redirector_vpc_cidr, 8, 1)
   availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
 
